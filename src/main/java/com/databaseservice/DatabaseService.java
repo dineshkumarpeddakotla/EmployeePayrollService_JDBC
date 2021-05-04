b22d21ddfb58eebd87cbb72d695f12355b90cdc2
dinnu247 <dineshkumar.icon@gmail.com>
2021-05-04 20:59:47 +0530

    CREATED: DatabaseService Class to Connect Database

---
+++ src/main/java/com/databaseservice/DatabaseService.java
@@ -0,0 +1,14 @@
+package com.databaseservice;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.util.Enumeration;
+
+import static java.lang.Class.forName;
+
+public class DatabaseService {
+    public static void main(String[] args) {
+
+    }
+}
